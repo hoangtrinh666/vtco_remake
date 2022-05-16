790ff8656e0655bec98b51bbd4daa36bdd7203ad
hoangtrinh666 <trinhhoang159357@gmail.com>
2022-05-16 17:37:51 +0700

    hello

--- js/index.js
+++ js/index.js
@@ -23,9 +23,9 @@ $(document).on("click", ".alert-modal .alert", function () {
 $(document).ready(function () {
   var lastId,
     topMenu = $(".navbar-nav"),
-    topMenuHeight = topMenu.outerHeight()+15,
+    topMenuHeight = topMenu.outerHeight()+40,
     // All list items
-    menuItems = topMenu.find("a"),
+    menuItems = topMenu.find(".nav-link"),
     // Anchors corresponding to menu items
     scrollItems = menuItems.map(function(){
       var item = $($(this).attr("href"));
@@ -51,21 +51,25 @@ $(document).ready(function () {
   });
   // Bind to scroll
   $(window).scroll(function () {
-    // Get container scroll position
-    var fromTop = $(this).scrollTop() + topMenuHeight;
-
-    // Get id of current scroll item
-    var cur = scrollItems.map(function () {
-      if ($(this).offset().top < fromTop) return this;
-    });
-    // Get the id of the current element
-    cur = cur[cur.length - 1];
-    var id = cur && cur.length ? cur[0].id : "";
-    // Set/remove active class
-    menuItems
-      .removeClass("active")
-      .filter("[href='#" + id + "']")
-      .addClass("active");
+       // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }  
   });
   var $owl = $("#service__slide");
 
