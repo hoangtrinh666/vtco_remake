afff2bcf17771c53e70331a7b6ac1b93b9b03838
hoangtrinh666 <trinhhoang159357@gmail.com>
2022-05-18 12:00:13 +0700

    hello

--- js/blog.js
+++ js/blog.js
@@ -22,66 +22,6 @@ $(document).on("click", ".alert-modal .alert", function () {
 });
 
 $(document).ready(function () {
-  (function ($, window) {
-    $("[data-toggle]").on("click", function (event) {
-      event.preventDefault();
-      var target = $(this.hash);
-      target.toggle();
-    });
-  
-    // Cache selectors
-    var lastId,
-      topMenu = $(".navbar-nav"),
-      topMenuHeight = topMenu.outerHeight() + 30,
-      // All list items
-      menuItems = topMenu.find(".nav-link"),
-      // Anchors corresponding to menu items
-      scrollItems = menuItems.map(function () {
-        var item = $(this).attr("href");
-        if (item != "#") {
-          return $(item);
-        }
-      });
-  
-    console.log(scrollItems);
-  
-    // Bind click handler to menu items
-    // so we can get a fancy scroll animation
-    menuItems.click(function (e) {
-      var href = $(this).attr("href"),
-        offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
-      $("html, body").stop().animate(
-        {
-          scrollTop: offsetTop,
-        },
-        300
-      );
-    });
-    // Bind to scroll
-    $(window).scroll(function () {
-      // Get container scroll position
-      var fromTop = $(this).scrollTop() + topMenuHeight;
-  
-      // Get id of current scroll item
-      var cur = scrollItems.map(function () {
-        if ($(this).offset().top < fromTop)
-          // console.log(this)
-          return this;
-      });
-      // Get the id of the current element
-      cur = cur[cur.length - 1];
-      var id = cur && cur.length ? cur[0].id : "";
-  
-      if (lastId !== id) {
-        lastId = id;
-        // Set/remove active class
-        menuItems
-          .removeClass("active")
-          .filter("[href='#" + id + "']")
-          .addClass("active");
-      }
-    });
-  })($, window);
   $(".navbar-toggler").click(function () {
     $(".overlay").addClass("show-overlay");
   });
